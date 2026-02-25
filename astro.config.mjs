b05c407809b22e0c13ee395107274e3c9765d8aa
John Sherrell <dev@ohn.sh>
2026-02-25 11:24:07 -0600

    Added starlight. Markdown in src/content/docs will be mapped to routes at /

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +16,25 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), starlight({ title: '🧪 scratch' })]
 });
+
+/** @typedef {import('@astrojs/starlight/types').StarlightUserConfig} StarlightUserConfig */
+/** @type {StarlightUserConfig} */
+/* starlight({
+	title: 'My Docs',
+	social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+	sidebar: [
+		{
+			label: 'Guides',
+			items: [
+				// Each item here is one entry in the navigation menu.
+				{ label: 'Example Guide', slug: 'guides/example' },
+			],
+		},
+		{
+			label: 'Reference',
+			autogenerate: { directory: 'reference' },
+		},
+	],
+}) */
