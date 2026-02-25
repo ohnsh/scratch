d7fe31d9a59a8eb88bb9672279105f9e6cbef7da
John Sherrell <dev@ohn.sh>
2026-02-24 23:37:45 -0600

    Added shadcn/ui, react, and tailwind. Split base layout; one for tailwind and another for open-props. Import aliases.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    env: {
-        schema: {
-            YT_API_KEY: envField.string({ context: "server", access: "secret" }),
-        }
-    }
+  env: {
+      schema: {
+          YT_API_KEY: envField.string({ context: "server", access: "secret" }),
+      }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
