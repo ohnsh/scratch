ebdedb367b1c8523fc27bb6fcd805f8c84b0e508
John Sherrell <dev@ohn.sh>
2026-03-05 12:46:00 -0600

    Shim docs content loader to generate redundant sidebar frontmatter options.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,34 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from 'astro:content'
+import type { Loader } from 'astro/loaders'
+import { docsLoader } from '@astrojs/starlight/loaders'
+import { docsSchema } from '@astrojs/starlight/schema'
 
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-};
+// Trying to avoid redundant frontmatter. Idk about mutating the sidebar object. Needs work.
+const loader: Loader = {
+    name: 'shim',
+    load: async (context) => {
+        await docsLoader().load(context)
+        context.store
+            .values()
+            .filter(({ id, data: { date} }) => id.startsWith('2026') && date)
+            .forEach(({ data }) => {
+				const { date, sidebar } = data as { date: Date, sidebar: {} }
+                const localDate = new Date(date)
+                localDate.setMinutes(date.getMinutes() + date.getTimezoneOffset())
+				sidebar.order = 31 - localDate.getDate()
+				sidebar.label = localDate.toLocaleString('en-US', { month: 'short', day: 'numeric' })
+                // data.date = localDate
+            })
+    },
+}
+
+const opts: Parameters<typeof docsSchema>[0] = {
+    extend: z.object({
+		date: z.date().optional()
+	})
+	// }).transform(({ date }) => ({
+	// 	date: date?.toISOString(),
+	// })),
+}
+
+export const collections = { docs: defineCollection({ loader, schema: docsSchema(opts) }) }
